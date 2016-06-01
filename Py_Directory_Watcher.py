d7e3388bf85cdeb86fff2761f0ee0a01aa7ddb36
Trevor <tgahl7@gmail.com>
2016-06-01 13:47:50 -0600

    Optimized code and added comments

--- Py_Directory_Watcher.py
+++ Py_Directory_Watcher.py
@@ -4,7 +4,6 @@ Created on Tue May 31 12:43:43 2016
 
 @author: Trevor Gahl
 """
-#import os
 import sys
 import time
 
@@ -14,36 +13,40 @@ from watchdog.observers import Observer
 from auth import authenticate
 from upload import upload_kitten
 
-
+#Authorizes connection with Borealis Imgur account
 client = authenticate()
 
 
 """
-Directory Watcher
+Directory Watcher and Imgur Uploader
 """
 
 
- 
+#Creates a handler capable of matching patterns to watch the directory 
 class MyHandler(PatternMatchingEventHandler):
+    #Checks for any file with a .jpg or .png file extension
     patterns = ["*.jpg", "*.png"]
 
     def process(self, event):        
-        # the file will be processed there
         print event.src_path, event.event_type  # print now only for degug
 
+    #Checks if a file has been modified
     def on_modified(self, event):
         self.process(event)
         print "File Modified"
 
+    #Checks if a file has been created
     def on_created(self, event):
         self.process(event)
+        #If a new file was created it uploads the photo to the Borealis Imgur account
         image = upload_kitten(client, event.src_path)
-        print("Image was posted! Go check your images you sexy beast!")
+        print("Image was posted!")
         print("You can find it here: {0}".format(image['link']))
         
 if __name__ == '__main__':
     args = sys.argv[1:]
     observer = Observer()
+    #Modify path in following line of code to where the pictures will be stored.
     observer.schedule(MyHandler(), path=args[0] if args else 'C:\Users\Trevor\Documents')
     observer.start()
 
